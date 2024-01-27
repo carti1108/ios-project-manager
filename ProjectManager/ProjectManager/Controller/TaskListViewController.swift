e4b3fd73e965399b5853d25f0f246df5389b4deb
kiseok <carti1108@naver.com>
2024-01-27 18:18:28 +0900

    feat: ui구성 메서드 생성 및 호출

--- ProjectManager/ProjectManager/Controller/TaskListViewController.swift
+++ ProjectManager/ProjectManager/Controller/TaskListViewController.swift
@@ -1,3 +1,20 @@
     private enum Section {
         case main
     }
+        configureUI()
+        configureLayout()
+extension TaskListViewController {
+    private func configureUI() {
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(self.listCollectionView)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            self.listCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.listCollectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            self.listCollectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.listCollectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
