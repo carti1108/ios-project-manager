9fb3c2395eb3bc1e34dc8a2ea5ad0d052b0d5b40
kiseok <carti1108@naver.com>
2024-01-27 18:20:43 +0900

    feat: datasource 프로퍼티 생성 및 datasource 구성을 위한 메서드 생성 및 구현

--- ProjectManager/ProjectManager/Controller/TaskListViewController.swift
+++ ProjectManager/ProjectManager/Controller/TaskListViewController.swift
@@ -10,9 +10,60 @@
         
         return collectionView
     }()
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Todo>?
     private var taskData: [Todo]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         configureUI()
         configureLayout()
+        configureDataSource()
+        configureSnapshot()
+    }
+extension TaskListViewController {
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<TaskListCell, Todo> { cell, indexPath, itemIdentifier in
+            guard let taskData = self.taskData else {
+                return
+            }
+            
+            cell.configure(taskData[indexPath.row])
+        }
+        
+        let headerRegistration = UICollectionView.SupplementaryRegistration<TaskHeaderView>(elementKind: TaskListViewController.sectionHeaderElementKind) { supplementaryView, elementKind, indexPath in
+            supplementaryView.prepare(text: "TODO", count: 5)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: listCollectionView,
+                                                        cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+            
+            return cell
+        })
+        
+        dataSource?.supplementaryViewProvider = { view, kind, index in
+            return self.listCollectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: index)
+        }
+    }
+    
+    private func configureSnapshot() {
+        guard let taskData = taskData else {
+            return
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Todo>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(taskData)
+        
+        dataSource?.apply(snapshot)
+    }
+}
+
 extension TaskListViewController {
     private func configureUI() {
         self.view.backgroundColor = .systemBackground
