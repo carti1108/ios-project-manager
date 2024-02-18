45a54c1865d00af18a9c1958541e310c9a4c08b0
kiseok <carti1108@naver.com>
2024-02-18 21:46:13 +0900

    feat: 프로퍼티 생성

--- ProjectManager/ProjectManager/Sources/Controller/AddTaskViewController.swift
+++ ProjectManager/ProjectManager/Sources/Controller/AddTaskViewController.swift
@@ -7,7 +7,28 @@
 
 import UIKit
 
-class AddTaskViewController: UIViewController {
+final class AddTaskViewController: UIViewController {
+    private let titleTextField: UITextField = {
+        let textField: UITextField = .init()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Title"
+        
+        return textField
+    }()
+    private let dueDatePicker: UIDatePicker = {
+        let datePicker: UIDatePicker = .init()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        return datePicker
+    }()
+    private let descriptionTextView: UITextView = {
+        let textView: UITextView = .init()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
